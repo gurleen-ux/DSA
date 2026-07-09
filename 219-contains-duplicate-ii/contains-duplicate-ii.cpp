a2d29ca8f1c0db4cec72c153dffc54a844ce37bf
gurleen_957 <deep07gurleen@gmail.com>
2026-07-09 11:02:43 +0530

    Time: 62 ms (97.10%) | Memory: 98.8 MB (34.45%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,17 +4,10 @@ public:
         unordered_map<int,int> mp;
         
         for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i]) != mp.end()){
-               if(i- mp[nums[i]] <= k){
+            if((mp.find(nums[i]) != mp.end()) && (i- mp[nums[i]] <= k)){
                 return true;
                }
-               else{
-                mp[nums[i]]=i;
-               }
-            }
-            else{
-                mp[nums[i]]=i;
-            }
+               else mp[nums[i]]=i; 
         }
         return false;
     }
