a3902448103c85e0d1ad73016de16792dca42501
gurleen_957 <deep07gurleen@gmail.com>
2026-08-13 10:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.00%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int ss(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+       }
+    return sum;
+    }
+    bool isHappy(int n) {
+        int slow= ss(n);
+        int fast= ss(ss(n));
+        while(slow!= fast){
+            slow= ss(slow);
+            fast= ss(ss(fast));
+        }
+        return slow==1;
+    }
+};
